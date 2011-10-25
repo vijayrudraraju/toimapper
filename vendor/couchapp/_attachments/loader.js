a394e05592861baed3392d63860dbc21d9b10c01
Vijay Rudraraju <vijay326@gmail.com>
2011-10-25 18:40:52 -0400

    upgrade processing

--- vendor/couchapp/_attachments/loader.js
+++ vendor/couchapp/_attachments/loader.js
@@ -14,6 +14,6 @@ couchapp_load([
   "vendor/couchapp/jquery.couch.app.util.js",
   "vendor/couchapp/jquery.mustache.js",
   "vendor/couchapp/jquery.evently.js",
-  "vendor/processingjs/processing-1.3.0-api.js",
+  "vendor/processingjs/processing-1.3.6-api.js",
   "vendor/dsp/dsp.js"
 ]);
